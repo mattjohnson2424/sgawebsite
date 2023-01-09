0607bb7a5a9f70b44b4a0519306d0b4d386a7393
mattjohnson2424 <mattjohnson@elcachargers.org>
2023-01-08 21:25:09 -0500

    lock icon added to cloud

--- src/components/events/AttendanceLocked.js
+++ src/components/events/AttendanceLocked.js
@@ -7,7 +7,7 @@ export const AttendanceLocked = memo(({ locked }) => {
         <>
             {locked && 
                 <div className="attendance-locked">
-                    <img className="lock-icon" src="/media/lock-icon.png" alt="lock"/>
+                    <img className="lock-icon" src="/https://firebasestorage.googleapis.com/v0/b/elcastudentgovernment.appspot.com/o/media%2Flock.png?alt=media&token=d576fb31-f880-4ab2-b0e6-f32d93724680" alt="lock"/>
                 </div>
             }
         </>
